fbf7dbc7e8ae006eb30abeca73cd9f911534cc85
mukes <mukeshsingh1407@gmail.com>
2024-06-03 17:10:47 +0530

    Passing Command lines Arguments and main methods

---
+++ src/main/java/org/practice/cl/Test.java
@@ -0,0 +1,19 @@
+package org.practice.cl;
+
+public class Test
+{
+    public static void main(String[] args)
+
+    {
+
+        for (int i = 0; i < args.length; i++)
+        {
+
+            System.out.println("args ["+i+"]="+args[i]);
+
+        }
+
+
+
+    }
+}
